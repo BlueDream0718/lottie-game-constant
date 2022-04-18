7bd3adc0db5a72f299cbcbdf27ec581763451e3d
superdeveloper718 <103200988+topdevmaster@users.noreply.github.com>
2022-04-18 02:48:28 -0800

    Update BaseShot.js

--- src/components/BaseShot.js
+++ src/components/BaseShot.js
@@ -16,7 +16,7 @@ let backAudio = loadSound('bMusic', true)
 backAudio.loop = true;
 backAudio.volume = 0.2;
 
-let currentSceneNumber = 9;
+let currentSceneNumber = 11;
 
 let letterVoiceList = [
     '16', '17',
